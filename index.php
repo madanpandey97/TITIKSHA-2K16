29911e91bdf8ee4c41afce0209267339ec12512a
madan <madanpandey410@gmail.com>
2016-09-28 00:41:12 +0530

    main index page

---
+++ index.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+	<meta charset="UTF-8">
+	<title>Titiksha 2016</title>
+	<!-- meta tags  -->
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="description" content="the annual techfest of shri mata vaishno devi university,the largest techfest of jammu&  kashmir,computer science, mathematics, electronics, machenical, biotechnology,physics, mathematics.">
+	<meta name="keywords" content="TITIKSHA, titiksha 2k16,Annual techfest,technical techfest,shri mata vainshno devi unversity,technical festival organized by shri mata vaishno devi university with vision og bogging the mind of young  tech . savied around the nation. ">
+    <meta http-equiv="cache-control" content="public" />    
+	<meta name="author" content="madan pandey, anoop singh, amit kumar yadav">
+	 <!-- meta tags end -->
+    <link rel="shortcut icon" href="img/favicon.ico">
+	<!--<link rel="stylesheet" type="text/css" href="css/background.css">-->
+	<link type="text/css" rel="stylesheet" href="css/materialize1.css"  media="screen,projection"/>	      
+    <link type="text/css" rel="stylesheet" href="css/madan.css"/>  
+      
+        <!-- Use laurenz.js to edit particles.js config -->
+    <script src="js/particles.js"></script>
+        
+	<link rel="stylesheet" type="text/css" href="css/layout.css">
+	<link rel="stylesheet" type="text/css" href="css/hover.css">
+	<link rel="stylesheet" type="text/css" href="css/modal.css">
+	
+	<link rel="stylesheet" type="text/css" href="css/countdown.css">
+	<link rel="stylesheet" href="preloader/css/normalize.css">
+	<link rel="stylesheet" href="preloader/css/main.css">
+	
+	<script src="preloader/js/vendor/modernizr-2.6.2.min.js"></script>
+	
+	<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">-->
+
+</head>
+<body class="green-mint">
+ <div id="particles-js" class="green-wine"></div>     
+	<!--	<div id="background_container">
+      <canvas height="610" width="1349" id="background"></canvas>
+  </div>
+  
+
+  <script src="js/background.js"></script>	
+  -->
+
+<div id="home" class="cotainer" width:100%; height:643px; z-index:1000>
+                                                                       
+<div class="container" style="z-index:1; float:left; position:absolute; left:0; top:0;">
+<a href="http://www.smvdu.ac.in//" target="_blank"><img src="img/2.png" height="100px" width="100px"></a>
+</div>
+<div class="container" style="z-index:1; text-align:right; color:white; font-family:'Montserrat', sans-serif; width:11%; float:left; position:absolute; left:5px;; top:20px;">
+<div id="loader-wrapper">
+			<div id="loader"></div>
+
+			<div class="loader-section section-left"></div>
+            <div class="loader-section section-right"></div>
+
+		</div>
+
+
+		<!--modal-->
+        <div class="remodal-bg">
+            <div class="remodal" data-remodal-id="modal1">
+             <h3>Titiksha 2k16, the largest techfest in Jammu and Kashmir, is the annual technical festival of Shri Mata Vaishno Devi University. It contains various competitions, workshops, guest lectures, exhibitions and gaming events.
+</h3> 
+
+             
+          </div>
+        </div>
+        <div class="remodal-bg">
+            <div class="remodal" data-remodal-id="modal2">
+             <br>
+          </div>
+        </div>
+        <div class="remodal-bg">
+            <div class="remodal" data-remodal-id="modal3">
+            
+              <br>
+          </div>
+        </div>
+        <div class="remodal-bg">
+            <div class="remodal" data-remodal-id="modal4">
+		</div>
+        </div>  
+        <div class="remodal-bg">
+            <div class="remodal" data-remodal-id="modal6">
+			
+		
+		</div>
+        </div>  
+    <!--modal ends-->
+	<!--<div class="bg-svg"><object><embed src="bg/bg.svg"></object></div>-->
+	<div class="old-wrap seen">
+<!--**********************************************************************************************************************************************-->	
+		<ul id="countdown" style="top:1%;">
+			<li>
+				<span class="days">00</span>
+				<p class="timeRefDays">days</p>
+			</li>
+			<li>
+				<span class="hours">00</span>
+				<p class="timeRefHours">hours</p>
+			</li>
+			<li>
+				<span class="minutes">00</span>
+				<p class="timeRefMinutes">min</p>
+			</li>
+			<li>
+				<span class="seconds">00</span>
+				<p class="timeRefSeconds">sec</p>
+			</li>
+		</ul>
+			
+			
+			
+		
+
+				
+				
+			</ul>
+
+			
+				
+			<div id="canvasContainer">
+
+				
+			</div>
+
+			<div class="widt">
+				<a href="https://www.youtube.com/channel/UC9Pwrxrw0asIpivtLiOwmSA" target="_blank"><img src="images/icons/yt.svg" class="w-icon"></a>
+				<a href="https://www.facebook.com/Titksha.smvdu/?fref=ts" target="_blank"><img src="images/icons/fb.svg" class="w-icon"></a>
+
+			</div>
+<!--***********************************************S P O N S O R S  S L I D E R*************************************************************-->	
+<!--
+<div id="slideshow">
+    			 <div id="slideshowWindow">
+    
+        			<div class="slide">
+            			<img src="images/works/tbic.jpg" />
+        			</div>
+        			
+        			<div class="slide">
+            			<img src="images/works/tbic.jpg" id="spider" />
+        			</div>
+
+        			<div class="slide">
+            			<img src="images/works/tbic.jpg" />
+        			</div>   
+                                <div class="slide">
+            			<img src="images/works/tbic.jpg" />
+        			</div>     
+        
+    			</div>
+   			</div>	-->
+
+<!--**********************"*************************S P O N S O R S  S L I D E R*******************************************************************-->			
+		<!--<object><embed src="images/bg/xship.svg" class="ui"></object>
+		<img src="images/bg/screen.svg" id="ui">-->
+		<a class="box" href="register/"><span>MENU</span></a>
+		<div class="body">
+		<div id="2logo">
+			
+		</div>
+
+		<br>
+		<div style="margin-bottom:10px;"></div>
+		<img src="img/LOG.png" left=40% style="width:75%; height:300px; margin-top:50px;float:center;" position: static;
+
+position: absolute;
+>
+	
+		
+		<br>
+		
+		<!--<div>Oct 30 - Nov 1</div>-->
+		</div>
+
+		
+	</div>
+	<!--*******************************DASHBOARD**********************************************************-->
+	
+	
+	 	<script type="text/javascript" src="js/materialize.js"></script>        
+        <script src='js/scrollReveal.min.js'></script>
+        <script src="js/particles.js"></script>        
+        <script src="js/laurenz.js"></script>
+	
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="preloader/js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+	<script src="preloader/js/main.js"></script>
+
+	<script type="text/javascript" src="js/prefixfree.min.js"></script>
+	<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+	<script src="js/modal.js"></script>
+	
+	<script type="text/javascript" src="js/countdown.js"></script>
+	<script type="text/javascript">
+	$(document).ready(function() {
+	
+		$("#countdown").countdown({
+			date: "18 november 2016 00:00:00", 
+			format: "on" 
+		},
+		
+		function() { 
+			
+			// the code here will run when the countdown ends
+			 
+
+		});
+	});
+</script>
+
+<script type="text/javascript">
+	var glower = $('.myGlower');
+window.setInterval(function() {  
+    glower.toggleClass('active');
+}, 1000);
+</script>
+    </body>
+</html>
